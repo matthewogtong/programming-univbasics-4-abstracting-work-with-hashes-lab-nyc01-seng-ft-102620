5662e783c83a336d9f65c7513b07bfe9bfc665c3
Matthew Ogtong <ogtongm@gmail.com>
2020-10-05 17:15:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  {
-    key => hash
-  }
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
